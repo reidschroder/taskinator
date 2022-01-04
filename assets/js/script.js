b80d3cf92b6721b77e862845ebdb5db7401e1bcc
Reid Schroder <reidschroder2@gmail.com>
2022-01-04 17:02:28 -0500

    added event prevent so page wont refresh on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //replaced console.log for expressions tha create a new task item, style the new task item, add text, and append the element to the task list. This is KEY to dynamically creating elements with the DOM
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) { 
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li"); 
     listItemEl.className = "task-item"; 
     listItemEl.textContent = "This is a new task."; 
     tasksToDoEl.appendChild(listItemEl); 
     }; 
   
-  buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
